ed3bc8a0cc8c75440d684e04a5cbee88e8c43088
Jan Sitarski <jansitarski00@gmail.com>
2022-08-31 13:38:04 +0200

    feat: copy file to bucket

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-
 mc alias set deploy $MINIO_ENDPOINT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY --api S3v4
-echo "deploy to: $1 deploy/$2"
-ls $1
-mc mirror --overwrite $1 "deploy/$2"
-echo "URL to download."
-url=$(mc share download deploy/$2$FILE_TO_DOWNLOAD  --json | jq -r '.share')
-echo $url
-curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"$BOT\", \"text\": \"$MSG $url\", \"icon_emoji\": \":rocket:\"}" $SLACK_HOOK
+ls -al
+echo "deploy to: $SOURCE_FILE deploy/$TARGET_DIR"
+mc cp --recursive $SOURCE_FILE "deploy/"$TARGET_DIR
+#echo "URL to download."
+#url=$(mc share download deploy/$2$FILE_TO_DOWNLOAD  --json | jq -r '.share')
+#echo $url
+#curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"$BOT\", \"text\": \"$MSG $url\", \"icon_emoji\": \":rocket:\"}" $SLACK_HOOK
