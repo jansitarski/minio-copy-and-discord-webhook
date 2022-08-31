15eed151987592611b78a5b69bd5b6a8aa0cd520
Jan Sitarski <jansitarski00@gmail.com>
2022-08-31 15:42:07 +0200

    fix: bash webhook check

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,13 @@ mc alias set deploy $MINIO_ENDPOINT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY --api S3
 echo "deploy to: $SOURCE_FILE deploy/$TARGET_DIR"
 mc cp --recursive $SOURCE_FILE "deploy/"$TARGET_DIR
 
-if($WEBHOOK_URL!="error"){
+if [ $WEBHOOK_URL != 'error' ]
+then
   echo "Send webhook $WEBHOOK_URL"
   curl -H "Content-Type: application/json" \
           -d '{"username": "MinIO",  "embeds": [{
               "title": $WEBHOOK_TITLE,
               "description": $WEBHOOK_DESC
               }]}' $WEBHOOK_URL
-}
+fi
 
