daffa6a4707d91f3c2a23d4700f5e753ac1270a1
Jan Sitarski <jansitarski00@gmail.com>
2022-08-31 15:10:02 +0200

    feat: discord webhook

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-mc alias set deploy $MINIO_ENDPOINT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY --api S3v4
-ls -al
+#mc alias set deploy $MINIO_ENDPOINT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY --api S3v4
+curl -H "Content-Type: application/json" -d '{"username": "test", "content": "hello"}' $WEBHOOK_URL
 echo "deploy to: $SOURCE_FILE deploy/$TARGET_DIR"
-mc cp --recursive $SOURCE_FILE "deploy/"$TARGET_DIR
-#echo "URL to download."
-#url=$(mc share download deploy/$2$FILE_TO_DOWNLOAD  --json | jq -r '.share')
-#echo $url
-#curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"$BOT\", \"text\": \"$MSG $url\", \"icon_emoji\": \":rocket:\"}" $SLACK_HOOK
+echo "Send webhook $WEBHOOK_URL"
+#mc cp --recursive $SOURCE_FILE "deploy/"$TARGET_DIR
