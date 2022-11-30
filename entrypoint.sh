5adec6458be7468f8a472ec64658b9629ec2579b
Jan Sitarski <56561977+jansitarski@users.noreply.github.com>
2022-11-30 11:54:10 +0100

    Fail feature (#2)
    * test: debug permission loss
    * feat: fail if arg not given

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
+if [ -z "$MINIO_ENDPOINT" ]
+  then
+    echo "No endpoint specified"
+    exit 1
+fi
+if [ -z "$MINIO_ACCESS_KEY" ]
+  then
+    echo "No access key specified"
+    exit 1
+fi
+if [ -z "$MINIO_SECRET_KEY" ]
+  then
+    echo "No access key specified"
+    exit 1
+fi
+
 mc alias set deploy $MINIO_ENDPOINT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY --api S3v4
+
 echo "deploy to: $1 deploy/$2"
-mc cp --recursive $1 "deploy/"$2
+mc cp --debug --recursive $1 "deploy/"$2
 
 if [ $WEBHOOK_URL != 'error' ]
 then
